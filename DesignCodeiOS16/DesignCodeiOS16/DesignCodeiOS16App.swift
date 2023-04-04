181331e42cd7c8f49859698d7352da91a4ef6831
Seongbeom Hong <sb12@tuta.io>
2023-04-04 18:23:14 +0900

    Build SwiftUI Apps
    Learned about
    - Basic layout
    - SF width styles and gradients
    - Variable color and animation
    - NavigationStack
    - NavigationDestination

---
+++ DesignCodeiOS16/DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by Steve Hong on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
